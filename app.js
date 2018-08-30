f327ec8f4470e5fdbd60e14f14fe01195f068324
AJ <aj.nice@kakaocorp.com>
2018-08-30 18:00:38 +0900

    keydown발생시마다 맞는 오디오dom을 찾아서 play()

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 //js
 
-function playSound(){
+function playSound(e) {
+  //e.keyCode는 keydown시에  keyCode값이 들어감
+  console.log(e.keyCode);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
 
-  
+  audio.currentTime = 0;
+  audio.play();
 }
 
 window.addEventListener('keydown', playSound);
